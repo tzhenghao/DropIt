69b0b8e86d4d672498bd598feee99b0dea2c285b
Zheng Hao Tan <tanzhao@umich.edu>
2015-07-11 00:51:46 -0500

    added randomly generated colored boxes

--- DropIt/DropItViewController.swift
+++ DropIt/DropItViewController.swift
@@ -19,4 +19,41 @@ class DropItViewController: UIViewController {
         
         return CGSize(width: size, height: size)
     }
+    @IBAction func drop(sender: UITapGestureRecognizer) {
+        drop()
+    }
+    
+    func drop() {
+        var frame = CGRect(origin: CGPointZero, size: dropSize)
+        
+        frame.origin.x = CGFloat.random(dropsPerRow) * dropSize.width
+        
+        
+        let dropView = UIView(frame: frame)
+        dropView.backgroundColor = UIColor.random
+        
+        gameView.addSubview(dropView) 
+    }
+}
+
+private extension CGFloat {
+    static func random(max:Int) -> CGFloat {
+        return CGFloat(arc4random() % UInt32(max))
+    }
+}
+
+// Extension for random UIColor colors.
+private extension UIColor {
+    class var random: UIColor {
+        
+        switch arc4random() % 6 {
+            case 0: return UIColor.greenColor()
+            case 1: return UIColor.blueColor()
+            case 2: return UIColor.redColor()
+            case 3: return UIColor.orangeColor()
+            case 4: return UIColor.purpleColor()
+            case 5: return UIColor.yellowColor()
+            default: return UIColor.blackColor()
+        }
+    }
 }
